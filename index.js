3a7e36c2292a0b41dd2cbe9bb4c3b1a48fdcf590
shake-819 <132436249@gse.okayama-c.ed.jp>
2026-01-08 15:05:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -152,6 +152,15 @@ client.on("interactionCreate", async interaction => {
 
 // ====== 起動 ======
 console.log("LOGIN CALL");
+/
+
+client.on("debug", m => console.log("[DEBUG]", m));
+client.on("warn", m => console.log("[WARN]", m));
+client.on("error", e => console.error("[CLIENT ERROR]", e));
+client.on("shardError", e => console.error("[SHARD ERROR]", e));
+
+
+
 client.login(TOKEN);
 
 // ====== HTTP ======
